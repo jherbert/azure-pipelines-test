5f7554a2bf49cd06df343f8034209de0d74ba09e
Jason Herbert <jherbert@aplusfcu.org>
2018-09-14 10:20:12 -0500

    Solution and fake test

---
+++ test/azure-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace azure_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
